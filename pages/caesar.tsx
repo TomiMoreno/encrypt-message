2792ea1f193a47328f2ea07073b94e99929ce306
Tomás Canosa Moreno <tomimoreno03@gmail.com>
2022-07-28 20:37:22 -0300

    Add change encryption to caesar

--- pages/caesar.tsx
+++ pages/caesar.tsx
@@ -1,6 +1,198 @@
-import CaesarCipher from '../features/caesarCipher';
+import {
+  FormLabel,
+  FormControl,
+  FormErrorMessage,
+  Button,
+  NumberInput,
+  NumberInputField,
+  Heading,
+  Stack,
+  Text,
+  Textarea,
+  Select,
+  IconButton,
+  Flex,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import encrypt from "../features/caesarCipher/encrypt";
+import decrypt from "../features/caesarCipher/decrypt";
+import { ArrowUpDownIcon } from "@chakra-ui/icons";
 
-export default function Caesar(){
+export default function CaesarCipher() {
+  const [isEncrypting, setIsEncrypting] = useState(true);
+  return (
+    <Stack spacing={8}>
+      <Flex justifyContent="space-between" alignItems="center">
+        <Heading as="h1">Caesar Cipher</Heading>
+        <Flex gap={2}>
+          <FormLabel htmlFor="change-encryption">
+            {isEncrypting ? "Encrypt" : "Decrypt"} a message
+          </FormLabel>
+          <IconButton
+            aria-label={isEncrypting ? "Encrypt" : "Decrypt"}
+            icon={<ArrowUpDownIcon aria-label="Change" />}
+            colorScheme="blue"
+            size="sm"
+            onClick={() => setIsEncrypting((curr) => !curr)}
+            id="change-encryption"
+          />
+        </Flex>
+      </Flex>
+      {isEncrypting ? <EncryptComponent /> : <DecryptComponent />}
+    </Stack>
+  );
+}
 
-	return <CaesarCipher />
+function DecryptComponent() {
+  type FormValues = {
+    messageToDecrypt: string;
+    shift: number;
+  };
+  const [decryptedMessage, setDecryptedMessage] = useState("");
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<FormValues>();
+
+  function onSubmit(values: FormValues) {
+    try {
+      const decrypted = decrypt(values.messageToDecrypt, +values.shift);
+      setDecryptedMessage(decrypted);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Heading as="h2" size="lg" mb={4}>
+        Decrypt
+      </Heading>
+      <FormControl isInvalid={!!errors.messageToDecrypt}>
+        <FormLabel htmlFor="messageToDecrypt">Message to decrypt</FormLabel>
+        <Textarea
+          id="messageToDecrypt"
+          placeholder="Message to decrypt"
+          {...register("messageToDecrypt", {
+            required: "This is required",
+          })}
+        />
+        <FormErrorMessage>
+          {errors.messageToDecrypt && errors.messageToDecrypt.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      <FormControl isInvalid={!!errors.shift}>
+        <FormLabel htmlFor="shift">Shift used to encrypt</FormLabel>
+        <NumberInput>
+          <NumberInputField
+            id="shift"
+            placeholder="Shift"
+            {...register("shift", {
+              required: "This is required",
+              pattern: {
+                message: "Only positive or negative numbers are allowed",
+                value: /^[+-]?\d+$/,
+              },
+            })}
+          />
+        </NumberInput>
+        <FormErrorMessage>
+          {errors.shift && errors.shift.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      <Button mt={4} colorScheme="blue" isLoading={isSubmitting} type="submit">
+        Decrypt
+      </Button>
+
+      <Text
+        fontSize="xl"
+        fontWeight="bold"
+        color="blue.500"
+        mb={4}
+        textAlign="center"
+      >
+        {decryptedMessage}
+      </Text>
+    </form>
+  );
+}
+
+function EncryptComponent() {
+  type FormValues = {
+    messageToEncrypt: string;
+    shift: number;
+  };
+  const [encryptedMessage, setEncryptedMessage] = useState("");
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<FormValues>();
+
+  function onSubmit(values: FormValues) {
+    try {
+      const encrypted = encrypt(values.messageToEncrypt, +values.shift);
+      setEncryptedMessage(encrypted);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Heading as="h2" size="lg" mb={4}>
+        Encrypt
+      </Heading>
+      <FormControl isInvalid={!!errors.messageToEncrypt}>
+        <FormLabel htmlFor="messageToEncrypt">Message to encrypt</FormLabel>
+        <Textarea
+          id="messageToEncrypt"
+          placeholder="Message to encrypt"
+          {...register("messageToEncrypt", {
+            required: "This is required",
+          })}
+        />
+        <FormErrorMessage>
+          {errors.messageToEncrypt && errors.messageToEncrypt.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      <FormControl isInvalid={!!errors.shift}>
+        <FormLabel htmlFor="shift">Shift</FormLabel>
+        <NumberInput>
+          <NumberInputField
+            id="shift"
+            placeholder="Shift"
+            {...register("shift", {
+              required: "This is required",
+              pattern: {
+                message: "Only positive or negative numbers are allowed",
+                value: /^[+-]?\d+$/,
+              },
+            })}
+          />
+        </NumberInput>
+        <FormErrorMessage>
+          {errors.shift && errors.shift.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      <Button mt={4} colorScheme="blue" isLoading={isSubmitting} type="submit">
+        Encrypt
+      </Button>
+
+      <Text
+        fontSize="xl"
+        fontWeight="bold"
+        color="blue.500"
+        mb={4}
+        textAlign="center"
+      >
+        {encryptedMessage}
+      </Text>
+    </form>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,25 @@
-import Head from 'next/head'
-import NextLink from 'next/link'
-import { Box, Heading, LinkBox, LinkOverlay, Text, SimpleGrid} from '@chakra-ui/react'
+import Head from "next/head";
+import NextLink from "next/link";
+import {
+  Box,
+  Heading,
+  LinkBox,
+  LinkOverlay,
+  Text,
+  SimpleGrid,
+} from "@chakra-ui/react";
 
 export default function Home() {
-  
   return (
     <>
       <Heading as="h1" mb={4} textAlign="center">
         Encrypt Message
       </Heading>
+      <Text as="h5" fontSize={20} mb={4} textAlign="center">
+        Try some encryption algorithms and share secret messages!
+      </Text>
       <Heading as="h2" size="lg" mb={4}>
-        Methods
+        Algorithms
       </Heading>
       <SimpleGrid columns={[1, 1, 2]} spacing={8}>
         <NextLink href="/caesar">
@@ -34,20 +43,18 @@ export default function Home() {
               Transposition
             </Box>
             <Heading size="md" my="2">
-              <LinkOverlay href="#">
-                Columnar transposition cipher
-              </LinkOverlay>
+              <LinkOverlay href="#">Columnar transposition cipher</LinkOverlay>
             </Heading>
             <Text>
-              The Columnar transposition cipher is a transposition cipher where 
-              the message is written out in rows of a fixed length, and then read
-              out again column by column, and the columns are chosen in some
-              scrambled order. Both the width of the rows and the permutation
-              of the columns are usually defined by a keyword.
+              The Columnar transposition cipher is a transposition cipher where
+              the message is written out in rows of a fixed length, and then
+              read out again column by column, and the columns are chosen in
+              some scrambled order. Both the width of the rows and the
+              permutation of the columns are usually defined by a keyword.
             </Text>
           </LinkBox>
         </NextLink>
       </SimpleGrid>
     </>
-  );            
+  );
 }
